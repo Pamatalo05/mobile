f2cec97d4b71a453bca913e076799e56a883e57e
Paulo Tapia <pmtapia@espol.edu.ec>
2025-12-17 19:22:44 +0000

    fasf

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,6 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
